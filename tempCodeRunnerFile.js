be7fd81d4cee231e3c7dc39b45e7680859c6104d
KeivisonTech <keivisonwilliam65@gmail.com>
2026-05-25 11:02:46 -0300

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-const prompt = require('prompt-sync')();
-
-let lista = prompt('Digite números separados por espaço:').split(' ').map(Number);
-
-console.log(lista.sort((a, b) => a - b)); 
-console.log(lista.reduce((a, b) => a + b) / lista.length); 
-console.log(lista.length); 
